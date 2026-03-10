45748c075aca135cea90ceca72562cfa69bb2a49
hennoletto <henri.privatsat@gmail.com>
2026-03-10 15:26:17 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+let eunning = 0
+let minutes = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Ghost)
+    if (eunning == 1) {
+        eunning = 0
+    } else {
+        eunning = 1
+        basic.showNumber(minutes)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Angry)
+    basic.pause(5000)
+    basic.clearScreen()
+    music.stopAllSounds()
+})
+input.onGesture(Gesture.Shake, function () {
+    minutes = 0
+    eunning = 0
+    basic.showNumber(minutes)
+})
+basic.forever(function () {
+    if (eunning == 1) {
+        basic.pause(60000)
+        minutes += 1
+        basic.showNumber(minutes)
+    }
+    if (minutes == 1800000) {
+        eunning = 0
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Ringtone), music.PlaybackMode.UntilDone)
+    }
+})
